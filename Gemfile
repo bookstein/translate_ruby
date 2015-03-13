4a33f43f78b7f33a21bfbcf74e97f122b5cadc96
ebookstein <ebookstein@newrelic.com>
2015-03-12 18:12:29 -0700

    Add mysql gem. Create database connection to pairs database and model ruby-to-python data with RubyPair class.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'haml'
 gem 'activerecord', '3.0.20'
+gem 'mysql'
 ruby '2.1.3'
 
--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require 'active_record'
 
 set :root, File.dirname(__FILE__) # set root as parent directory of current file
 
+# model
+ActiveRecord::Base.establish_connection(
+  :adapter => "mysql",
+  :host => "localhost",
+  :database => "pairs",
+  :password => "Whatmeworry8"
+)
+
+class RubyPair < ActiveRecord::Base
+end
+
 # load tab-separated file as a hash
 translations = Hash.new
 datafile = File.open("translations.txt", "r").each do |line|
@@ -12,6 +23,11 @@ datafile = File.open("translations.txt", "r").each do |line|
   translations[ruby_method] = python_method
   translations
 end
+
+translations.each do |ruby, python|
+  RubyPair.create(:method => ruby, :python_method => python)
+end
+
 
 
 
