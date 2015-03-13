8b1ae5b2653310945057bdee4176a8bc1d3d9975
ebookstein <ebookstein@newrelic.com>
2015-03-12 17:18:43 -0700

    Require activerecord and include activerecord in Gemfile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'sinatra'
 gem 'haml'
+gem 'activerecord', '3.0.20'
 ruby '2.1.3'
 
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'haml'
+require 'active_record'
 
 set :root, File.dirname(__FILE__) # set root as parent directory of current file
 
